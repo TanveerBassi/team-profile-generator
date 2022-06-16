42a3fde6472bcc600c03396177f4bb74559ad351
Tanveer Bassi <tanveer13a551@gmail.com>
2022-06-16 15:34:32 +0100

    getanswers and writetofile updated

---
+++ src/utils/writeToFile.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const path = require("path");
+const renderHTML = require("./renderHTML");
+
+const writeToFile = (fileName, renderHTML) => {
+  const filePath = path.join(__dirname, `../../dist/${fileName}.html`);
+  console.log(filePath);
+
+  fs.writeFileSync(filePath, JSON.stringify(renderHTML));
+};
+
+module.exports = writeToFile;
